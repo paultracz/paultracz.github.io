f589743b8ab43c2673d20329a6deb3a458fa534d
PolzPT <paultracz07@gmail.com>
2021-04-18 15:48:05 -0400

    changed back to static page

--- src/main/java/ca/sheridancollege/repositories/StoreRepository.java
+++
@@ -1,9 +0,0 @@
-package ca.sheridancollege.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import ca.sheridancollege.beans.Store;
-
-public interface StoreRepository extends CrudRepository<Store, Integer> {
-
-}
